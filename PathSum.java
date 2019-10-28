23af8efeb95982a817d2277148dbff022a5d9cb6
Aili Gong <aili.hgong@gmail.com>
2019-10-28 00:34:37 +0000

    Added PathSum

---
+++ PathSum.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class PathSum {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        Stack<TreeNode> nodeStack = new Stack<TreeNode>();
+        Stack<Integer> sumStack = new Stack<Integer>();
+        nodeStack.add(root);
+        sumStack.add(sum - root.val);
+
+        while (!nodeStack.isEmpty()) {
+            TreeNode currentNode = nodeStack.pop();
+            int currentSum = sumStack.pop();
+
+            if (currentNode.left == null && currentNode.right == null && currentSum == 0) {
+                return true;
+            }
+            if (currentNode.left != null) {
+                nodeStack.add(currentNode.left);
+                sumStack.add(currentSum - currentNode.left.val);
+            }
+            if (currentNode.right != null) {
+                nodeStack.add(currentNode.right);
+                sumStack.add(currentSum - currentNode.right.val);
+            }
+        }
+        return false;
+    }
+}
