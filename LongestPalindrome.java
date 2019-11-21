26b0bb90651665044d9848c7d2df5d13e671c62a
Aili Gong <aili.hgong@gmail.com>
2019-11-21 04:55:14 +0000

    Added notes to longestPalindrome

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -7,9 +7,13 @@ public class LongestPalindrome {
         }
         int ans = 0;
         for (int v : count) {
+            // making a number into a even number; /2 will give floor number.
+            // 6 / 2 * 2 == 6, 7 / 2 * 2 == 6
             ans += v / 2 * 2;
             if (ans % 2 == 0 && v % 2 == 1) {
                 ans++;
+                // this if statement will only run at most once, because when ans++, it will be an odd
+                // number and will never satisfy ans % 2 == 0.
             }
         }
         return ans;
