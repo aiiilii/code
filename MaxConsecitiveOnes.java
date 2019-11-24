4ee140974916a7ae01c520a241454b366b8a52a2
Aili Gong <aili.hgong@gmail.com>
2019-11-24 05:12:47 +0000

    Added MaxConsecutiveOnes

--- MaxConsecitiveOnes.java
+++ MaxConsecitiveOnes.java
@@ -4,7 +4,13 @@ public class MaxConsecitiveOnes {
         int count = 0;
         int maxCount = 0;
         for (int i = 0; i < nums.length; i++) {
-            
+            if (nums[i] == 1) {
+                count++;
+                maxCount = Math.max(maxCount, count);
+            } else {
+                count = 0;
+            }
         }
+        return maxCount;
     }
 }
