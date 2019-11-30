dc32fe00c2ce3c4ce914178705068107bf71e5f1
Aili Gong <aili.hgong@gmail.com>
2019-11-30 23:54:23 +0000

    Finished BinaryTreeLevelOrderTraversalII

--- BinaryTreeLevelOrderTraversalII.java
+++ BinaryTreeLevelOrderTraversalII.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
@@ -12,6 +13,29 @@ public class BinaryTreeLevelOrderTraversalII {
     }
 
     public List<List<Integer>> levelOrderBorrom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
         
+        while(!q.isEmpty()) {
+            List<Integer> level = new ArrayList<Integer>();
+            int size = q.size();
+            for (int i = 0; i < size; i++){
+                TreeNode head = q.poll();
+                level.add(head.val);
+                if (head.left != null) {
+                    q.add(head.left);
+                }
+                if(head.right != null) {
+                    q.add(head.right);
+                }
+            }
+            res.add(0, level);
+        }
+        return res;
     }
 }
