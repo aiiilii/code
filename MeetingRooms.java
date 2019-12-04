6e72f1e0723e565d90add8cf55abaa8dee7fbfb3
Aili Gong <aili.hgong@gmail.com>
2019-12-04 05:53:27 +0000

    Added MeetingRooms

--- MeetingRooms.java
+++ MeetingRooms.java
@@ -1,6 +1,23 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class MeetingRooms {
 
     public boolean canAttendMeetings(int[][] intervals) {
-        
+        Arrays.sort(intervals, new SortIntArray());
+
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i][1] > intervals[i + 1][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private class SortIntArray implements Comparator<int[]> {
+
+        public int compare(int[] a, int[] b) {
+            return a[0] - b[0];
+        }
     }
 }
