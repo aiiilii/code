93f96f4af33d00923c0086dea1e273d5990f7e41
Aili Gong <aili.hgong@gmail.com>
2019-12-07 04:09:43 +0000

    Added HappyNumber

---
+++ HappyNumber.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+
+    public boolean isHappy(int n) {
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        Set<Integer> seen = new HashSet<Integer>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+
+    private int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+}
