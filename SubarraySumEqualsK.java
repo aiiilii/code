e6aa4c06d72e4ecf4734ae61cc1dd15db2c737a7
Aili Gong <aili.hgong@gmail.com>
2019-12-29 22:49:08 +0000

    Added two solutions for SubarraySumEqualsK

---
+++ SubarraySumEqualsK.java
@@ -0,0 +1,62 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+
+    /**
+     * HashMap Approach
+     * Time - O(n)
+     * Space - O(n)
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int sum = 0;
+
+        if (nums == null || nums.length == 0) {
+            return count;
+        }
+
+        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
+        mp.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (mp.containsKey(sum - k)) {
+                count += mp.get(sum - k);
+            }
+            mp.put(sum, mp.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
+    
+    /**
+     * Cummulative sum approach
+     * Time - O(n^2)
+     * Space - O(n)
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySum1(int[] nums, int k) {
+        int count = 0;
+        int[] sum = new int[nums.length + 1];
+        sum[0] = 0;
+
+        for (int i = 1; i <= nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
+        for (int start = 0; start < nums.length; start ++) {
+            for (int end = start + 1; end <= nums.length; end ++) {
+                if (sum[end] - sum[start] == k) {
+                    count ++;
+                }
+            }
+        }
+        return count;
+    }
+}
