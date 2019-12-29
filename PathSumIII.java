bcb93fae850c3c8761c7f34a0347d48570daf7a3
Aili Gong <aili.hgong@gmail.com>
2019-12-29 20:44:45 +0000

    Added 2nd solution to PathSumIII

--- PathSumIII.java
+++ PathSumIII.java
@@ -1,3 +1,6 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class PathSumIII {
 
     public class TreeNode {
@@ -7,6 +10,43 @@ public class PathSumIII {
         TreeNode(int x) { val = x; }
     }
 
+    /**
+     * Using Prefix Sum Array - fast
+     * @param root
+     * @param sum
+     * @return
+     */
+    public int pathSum1(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        Map<Integer, Integer> preSum = new HashMap<Integer, Integer>();
+        preSum.put(0, 1);
+
+        return helper(root, 0, sum, preSum);
+    }
+
+    private int helper(TreeNode root, int currSum, int target, Map<Integer, Integer> preSum) {
+        if (root == null) {
+            return 0;
+        }
+
+        currSum += root.val;
+        int res = preSum.getOrDefault(currSum - target, 0);
+        preSum.put(currSum, preSum.getOrDefault(currSum, 0) + 1);
+
+        res += helper(root.left, currSum, target, preSum) + helper(root.right, currSum, target, preSum);
+        preSum.replace(currSum, preSum.get(currSum) - 1);
+        return res;
+    }
+
+
+    /**
+     * Using multiple recursions - slow
+     * @param root
+     * @param sum
+     * @return
+     */
     public int pathSum(TreeNode root, int sum) {
         if (root == null) {
             return 0;
