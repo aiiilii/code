184aeb632b50dfdcf558c1c3c075163391eb2fee
Aili Gong <aili.hgong@gmail.com>
2020-01-03 02:33:37 +0000

    Added BFS solution to cloneGraph

--- CloneGraph.java
+++ CloneGraph.java
@@ -1,6 +1,8 @@
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.ArrayList;
 
 public class CloneGraph {
@@ -50,5 +52,48 @@ public class CloneGraph {
         return cloneNode;
     }
 
-    
+
+    /**
+     * BFS - using queue and hashmap both within function
+     * Time - O(n)
+     * Space - O(n)
+     * @param node
+     * @return
+     */
+    public Node cloneGraph1(Node node) {
+        if (node == null) {
+            return node;
+        }
+
+        // Hashmap tp save the visited node ant it's respective clone
+        // as key and value respectively. This helps to avoid cycles.
+        HashMap<Node, Node> visited = new HashMap<Node, Node>();
+
+        // Put the first node in the queue
+        Queue<Node> q = new LinkedList<Node>();
+        q.offer(node);
+
+        // Clone the node and put it in the visited dictionary.
+        visited.put(node, new Node(node.val, new ArrayList<Node>()));
+
+        // Start BFS traversal
+        while (!q.isEmpty()) {
+            // Pop a node ("n") from the front.
+            Node n = q.poll();
+            
+            // Iterate through all the neighbors of the node "n"
+            for (Node neighbor : n.neighbors) {
+                if (!visited.containsKey(neighbor)) {
+                    // Clone the neighbor and put in the visited, if not present already
+                    visited.put(neighbor, new Node(neighbor.val , new ArrayList<Node>()));
+                    // Add the newly encountered node to the queue
+                    q.offer(neighbor);
+                }
+                // Add the clone of the neighbor to the neighbors of the clone node "n"
+                visited.get(n).neighbors.add(visited.get(neighbor));
+            }
+        }
+        // Return the clone of the node from visited.
+        return visited.get(node);
+    }
 }
