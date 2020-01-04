4dae6759d61123781f2d0dcf29e7de59628ad1ea
Aili Gong <aili.hgong@gmail.com>
2020-01-04 04:36:30 +0000

    Added second solution to FindFirstAndLastPositionOfElementInSortedArray time - O(logn)

--- FindFirstAndLastPositionOfElementInSortedArray.java
+++ FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,5 +1,68 @@
 public class FindFirstAndLastPositionOfElementInSortedArray {
 
+    /**
+     * 2 binary searches
+     * Time - O(logn)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] searchRange1(int[] nums, int target) {
+        int[] res = new int[] {-1, -1};
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        res[0] = findLeft(nums, target);
+        res[1] = findRight(nums, target);
+        return res;
+    }
+    private static int findLeft(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) { // cannot be low <= high because no return to break the inifite loop if high = mid = low
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                high = mid; // move high to where mid is and keep searching until find the leftmost number that == target
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        if (nums[low] == target) {
+            return low;
+        }
+        return -1;
+    }
+    private static int findRight(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low + 1) / 2; //+1 because need the ceiling number for mid to avoid infinite loop
+            if (nums[mid] == target) {
+                low = mid;
+            } else if (target > nums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        if (nums[high] == target) {
+            return high;
+        }
+        return -1;
+    }
+    
+
+    /**
+     * 1 binary search and 2 while loops
+     * Time - O(log n) + O(n)
+     * @param nums
+     * @param target
+     * @return
+     */
     public static int[] searchRange(int[] nums, int target) {
         int[] res = new int[] {-1, -1};
         if (nums == null || nums.length == 0) {
