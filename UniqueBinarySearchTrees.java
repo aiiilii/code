99963b41e085008fb82a9918989a83e5d8fdc18c
Aili Gong <aili.hgong@gmail.com>
2020-01-13 06:24:18 +0000

    Updated UniqueBinarySearchTrees

--- UniqueBinarySearchTrees.java
+++ UniqueBinarySearchTrees.java
@@ -5,9 +5,11 @@ public class UniqueBinarySearchTrees {
         dp[0] = 1;
         dp[1] = 1;
 
-        for (int level = 2; level <= n; level ++) {
-            for (int root = 1; root <= level; root ++) {
-                dp[level] += dp[level - root] * dp[root - 1];
+        for (int rootNodes = 2; rootNodes <= n; rootNodes ++) {
+            for (int left = 0; left < rootNodes; left ++) {
+                // rootNodes = number of nodes; left = number of left nodes;
+                // thus number of right nodes = rootNodes - left - 1; 1 is for the root
+                dp[rootNodes] += dp[rootNodes - left - 1] * dp[left];
             }
         }
         return dp[n];
---
+++ UniqueBinarySearchTreesII.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniqueBinarySearchTreesII {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode (int x) { val = x; }
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        
+    }
+}
