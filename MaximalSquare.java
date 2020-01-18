796aea58fec15ff995e16a5b8fde45057ff30e5e
Aili Gong <aili.hgong@gmail.com>
2020-01-18 19:25:00 +0000

    Added MaximalSquare

---
+++ MaximalSquare.java
@@ -0,0 +1,30 @@
+public class MaximalSquare {
+
+    /**
+     * Time - O(m * n)
+     * Space - O(m * n)
+     * @param matrix
+     * @return
+     */
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int res = 0;
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    // dp[i][j] represent the edge length of the largest square ENDING at position(i, j)
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j - 1]), dp[i - 1][j]) + 1;
+                    res = Math.max(dp[i][j], res);
+                }
+            }
+        }
+        return res * res;
+    }
+}
