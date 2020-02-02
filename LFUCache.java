7b4ad877780e1ef0a093b1fcea8607c9909969a2
Aili Gong <aili.hgong@gmail.com>
2020-02-02 02:10:14 +0000

    Finished adding notes for LFUCache

--- LFUCache.java
+++ LFUCache.java
@@ -12,7 +12,7 @@ public class LFUCache {
         Node(int k, int v) {
             key = k;
             val = v;
-            count = 1;
+            count = 1; // this is the stored frequency count;
         }
     }
     
@@ -56,8 +56,8 @@ public class LFUCache {
         }
     }
 
-    Map<Integer, Node> mp;
-    Map<Integer, DDList> freq;
+    Map<Integer, Node> mp; // stores the key integer, and the node as value;
+    Map<Integer, DDList> freq; // stores the frequency integer, and the DLList of nodes that have that same frequency;
     int size;
     int capacity;
     int maxFreq;
@@ -75,18 +75,18 @@ public class LFUCache {
             return -1;
         }
         Node node = mp.get(key);
-        int prevFreq = node.count;
-        DDList prevList = freq.get(prevFreq);
-        prevList.remove(node);
+        int prevFreq = node.count; // get the frequency of the found node;
+        DDList prevList = freq.get(prevFreq); // get the DLList based on the prev frequency;
+        prevList.remove(node); // remove the node from the prev frequency list;
 
-        int currFreq = prevFreq + 1;
-        maxFreq = Math.max(maxFreq, currFreq);
-        DDList currList = freq.getOrDefault(currFreq, new DDList());
-        node.count ++;
-        currList.addHead(node);
+        int currFreq = prevFreq + 1; // frequency increments;
+        maxFreq = Math.max(maxFreq, currFreq); // compare the current freq to the maxFreq and store it as maxFreq;
+        DDList currList = freq.getOrDefault(currFreq, new DDList()); // get the curr frequency DLList, if not found, then create default empty new DLList();
+        node.count ++; // increment the stored node frequency count;
+        currList.addHead(node); // add the found node to the curr frequency DLList;
 
-        freq.put(prevFreq, prevList);
-        freq.put(currFreq, currList);
+        freq.put(prevFreq, prevList); // update the prevFreq DLList;
+        freq.put(currFreq, currList); // update the currFreq DLList;
         return node.val;
     }
 
@@ -94,32 +94,32 @@ public class LFUCache {
         if (capacity == 0) {
             return;
         }
-        if (mp.get(key) != null) {
-            mp.get(key).val = value;
-            get(key);
+        if (mp.get(key) != null) { // if that key already exist in the nodes map,
+            mp.get(key).val = value; // then only need to update the value;
+            get(key); // use get(key) to update the frequency lists
             return;
         }
 
-        Node node = new Node(key, value);
-        DDList currList = freq.getOrDefault(1, new DDList());
+        Node node = new Node(key, value); // if key does not exist, then need to create new node;
+        DDList currList = freq.getOrDefault(1, new DDList()); // this new node has just been visited once, thus find the DLList that have frequency of 1, if does not exist, then create empty new DLList;
 
         currList.addHead(node);
         size++;
 
-        if (size > capacity) {
-            if (currList.len > 1) {
+        if (size > capacity) { // then we need to delete a node
+            if (currList.len > 1) { // only delete from the frequency of 1 DLList if the currList.len > 1, because we don't want to delete the node that we just added in;
                 currList.removeTail();
-            } else {
-                for (int i = 2; i <= maxFreq; i++) {
-                    if (freq.get(i) != null && freq.get(i).len > 0) {
-                        freq.get(i).removeTail();
+            } else { // if currList.len == 1
+                for (int i = 2; i <= maxFreq; i++) { // need to find the next lowest frequency count that is bigger than 1
+                    if (freq.get(i) != null && freq.get(i).len > 0) { // if found and not null and length of found DLList is > 0,
+                        freq.get(i).removeTail(); // then remove tail from that list;
                         break;
                     }
                 }
             }
-            size --;
+            size --; // decrement size after deleting a node;
         }
-        freq.put(1, currList);
+        freq.put(1, currList); // update frequency of 1 DLList in the map;
     }
 
 }
