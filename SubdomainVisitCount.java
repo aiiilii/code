5456172021563476b650aad82e0e886e2d1eb5e8
Aili Gong <aili.hgong@gmail.com>
2020-02-03 03:28:47 +0000

    Finished notes for SubdomainVisitCount

--- SubdomainVisitCount.java
+++ SubdomainVisitCount.java
@@ -9,17 +9,17 @@ public class SubdomainVisitCount {
         Map<String, Integer> map = new HashMap<String, Integer>();
 
         for (String cd : cpdomains) {
-            int i = cd.indexOf(' ');
-            int n = Integer.valueOf(cd.substring(0, i));
-            String s = cd.substring(i + 1);
+            int i = cd.indexOf(' '); // find the index of the first space ' '
+            int n = Integer.valueOf(cd.substring(0, i)); // get the number of times visited
+            String s = cd.substring(i + 1); // s is everything after the number and the space
 
             for (i = 0; i < s.length(); i++) {
                 if (s.charAt(i) == '.') {
-                    String d = s.substring(i + 1);
+                    String d = s.substring(i + 1); // get everything after each dot '.', and put it in the map along with the number of times visited
                     map.put(d, map.getOrDefault(d, 0) + n);
                 }
             }
-            map.put(s, map.getOrDefault(s, 0) + n);
+            map.put(s, map.getOrDefault(s, 0) + n); // put all of s in to the map along with the number of times visited
         }
 
         List<String> res = new ArrayList<String>();
