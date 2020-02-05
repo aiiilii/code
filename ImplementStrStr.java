d56f1c6f1ffa27f46331b808475ffbb2205155b1
Aili Gong <aili.hgong@gmail.com>
2020-02-05 02:41:25 +0000

    Added ImplementStrStr

---
+++ ImplementStrStr.java
@@ -0,0 +1,37 @@
+public class ImplementStrStr {
+
+    public int strStr(String haystack, String needle) {
+        int N = needle.length();
+        int H = haystack.length();
+
+        if (N == 0) {
+            return 0;
+        }
+
+        int hayPos = 0;
+
+        while (hayPos < H - N + 1) { // length of the hay minus length of needle +1 is for index at 0;
+            while (hayPos < H - N + 1 && haystack.charAt(hayPos) != needle.charAt(0)) {
+                hayPos ++;
+            }
+
+            int currLen = 0;
+            int needlePos = 0;
+            while (needlePos < N && hayPos < H && haystack.charAt(hayPos) == needle.charAt(needlePos)) {
+                hayPos ++;
+                needlePos ++;
+                currLen ++;
+            }
+
+            // if the whole needle string is found,
+            // return its start position;
+            if (currLen == N) {
+                return hayPos - N;
+            }
+
+            // otherwize, backtrack and start over by finding the next char that equals to the needle.charAt(0);
+            hayPos = hayPos - currLen + 1;
+        }
+        return -1;
+    }
+}
