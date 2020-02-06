01127bc431f6f58585b704cbd21455bf85f259f5
Aili Gong <aili.hgong@gmail.com>
2020-02-06 02:46:19 +0000

    Changed names for DesignBoundedBlockingQueue

--- DesignBoundedBlockingQueue.java
+++ DesignBoundedBlockingQueue.java
@@ -6,34 +6,34 @@ public class DesignBoundedBlockingQueue {
 
     List<Integer> queue;
     Semaphore mutex;
-    Semaphore full;
     Semaphore empty;
+    Semaphore full;
 
     public DesignBoundedBlockingQueue(int capacity) {
         queue = new LinkedList<Integer>();
         mutex = new Semaphore(1); // can aquire before release
-        full = new Semaphore(0); // need to release then acquire
-        empty = new Semaphore(capacity); // can acquire the capacity number of times then block
+        empty = new Semaphore(0); // need to release then acquire
+        full = new Semaphore(capacity); // can acquire the capacity number of times then block
     }
 
     public void enqueue(int element) throws InterruptedException {
-        empty.acquire(); // -1 for acquire until capacity reaches 0
+        full.acquire(); // -1 for acquire until capacity reaches 0
 
         mutex.acquire();
         queue.add(element);
         mutex.release();
 
-        full.release(); // need to release so can be dequeued
+        empty.release(); // need to release so can be dequeued
     }
 
     public int dequeue() throws InterruptedException {
-        full.acquire();
+        empty.acquire();
 
         mutex.acquire();
         int ans = queue.remove(0);
         mutex.release();
 
-        empty.release();
+        full.release();
         return ans;
     }
 
