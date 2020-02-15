9b78596241c67ac640abd637fea31803dfb35910
Aili Gong <aili.hgong@gmail.com>
2020-02-15 19:49:13 +0000

    Added comments to DesignHitCounter

--- DesignHitCounter.java
+++ DesignHitCounter.java
@@ -3,11 +3,20 @@ public class DesignHitCounter {
     private int[] times;
     private int[] hits;
 
+    /**
+     * Using buckets, 1 bucket for every second because we only need to keep the recent hits info for 300 seconds.
+     * hit[] array is wrapped around by mod operation. Each hit bucket is associated with times[] bucket which record current time. 
+     * If it is not current time, it means it is 300s or 600s... ago and need to reset to 1.
+     */
     public DesignHitCounter() {
         times = new int[300];
         hits = new int[300];
     }
 
+    /**
+     * Time - O(1)
+     * @param timestamp
+     */
     public void hit(int timestamp) {
         int index = timestamp % 300;
         if (times[index] != timestamp) {
@@ -18,6 +27,11 @@ public class DesignHitCounter {
         }
     }
 
+    /**
+     * Time - O(s), s is the total seconds in given time interval, in this case, 300;
+     * @param timestamp
+     * @return
+     */
     public int getHits(int timestamp) {
         int total = 0;
         for (int i = 0; i < 300; i++) {
