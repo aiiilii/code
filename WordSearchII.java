f94ecf018c60c78086f2635c05dce4ddd4876432
Aili Gong <aili.hgong@gmail.com>
2020-02-17 02:54:20 +0000

    Finished notes for WordSearchII

--- WordSearchII.java
+++ WordSearchII.java
@@ -17,7 +17,7 @@ public class WordSearchII {
      */
     public List<String> findWords(char[][] board, String[] words) {
         List<String> res = new ArrayList<String>();
-        TrieNode root = buildTrie(words);
+        TrieNode root = buildTrie(words); // put all the String[] words into a trie;
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
@@ -47,25 +47,23 @@ public class WordSearchII {
         if (c == '#' || temp.children[c - 'a'] == null) {
             return;
         }
-        temp = temp.children[c - 'a'];
+        temp = temp.children[c - 'a']; // simulating index -> index + 1;
         if (temp.theWord != null) { // found one,
             res.add(temp.theWord);
-            temp.theWord = null; // de-duplicate;
+            temp.theWord = null; // de-duplicate, do not want to find the same word twice;
         }
 
         board[i][j] = '#';
-        if (i > 0) {
-            dfs(board, i - 1, j, temp, res);
+
+        int[] dx = new int[] {-1, 0, 0, 1};
+        int[] dy = new int[] {0, -1, 1, 0};
+
+        for (int k = 0; k < 4; k++) {
+            if ((i + dx[k] >= 0 && i + dx[k] < board.length) && (j + dy[k] >= 0 && j + dy[k] < board[0].length)) { // if in bounds, then go 4 directions;
+                dfs(board, i + dx[k], j + dy[k], temp, res);
+            }
         }
-        if (j > 0) {
-         dfs(board, i, j - 1, temp, res);
-        }
-        if (i < board.length - 1) {
-            dfs(board, i + 1, j, temp, res);
-        }
-        if (j < board[0].length - 1) {
-            dfs(board, i, j + 1, temp, res);
-        }
+
         board[i][j] = c;
     }
 }
