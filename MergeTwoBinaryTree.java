d768b7d8a0f03b1a52b23469f430f215a3fc45ee
Aili Gong <aili.hgong@gmail.com>
2020-02-27 23:52:30 +0000

    Added MergeTwoBinaryTree

---
+++ MergeTwoBinaryTree.java
@@ -0,0 +1,30 @@
+public class MergeTwoBinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    /**
+     * Recursive
+     * Time - O(m), m is the minimum number of nodes needed from the two given trees;
+     * Space - O(m), the depth of the recursion tree can go upto m in the case of a skewed tree, in average case, depth will be O(log m);
+     * @param t1
+     * @param t2
+     * @return
+     */
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        TreeNode root = new TreeNode(t1.val + t2.val);
+        root.left = mergeTrees(t1.left, t2.left);
+        root.right = mergeTrees(t1.right, t2.right);
+        return root;
+    }
+}
