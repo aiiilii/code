36d1cdcfa028c9723a3a3f472504f160cac94e46
Aili Gong <aili.hgong@gmail.com>
2020-03-11 23:41:55 +0000

    Added more concise solution

--- RemoveNthNodeFromEndOfList.java
+++ RemoveNthNodeFromEndOfList.java
@@ -6,6 +6,38 @@ public class RemoveNthNodeFromEndOfList {
         ListNode(int x) { val = x; }
     }
 
+    /**
+     * Time - O(L), L is the number of nodes, one traversal;
+     * Space - O(1), only used pointers;
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
+        if (head == null || n == 0) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        for (int i = 1; i <= n + 1; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+
+
+
     public ListNode removeNthFromEnd(ListNode head, int n) {
         if (head == null || head.next == null) {
             return null;
