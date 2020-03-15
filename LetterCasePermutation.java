6a6d8473a85f6fed019be5577dc3c391e1a92f18
Aili Gong <aili.hgong@gmail.com>
2020-03-15 00:26:03 +0000

    Added backtracking solution to LetterCasePermutation

--- LetterCasePermutation.java
+++ LetterCasePermutation.java
@@ -3,6 +3,41 @@ import java.util.List;
 
 public class LetterCasePermutation {
 
+    /**
+     * Backtracking
+     * @param S
+     * @return
+     */
+    public List<String> letterCasePermutation1(String S) {
+        if (S == null) {
+            return new ArrayList<String>();
+        }
+
+        List<String> res = new ArrayList<String>();
+        helper(S.toCharArray(), res, 0);
+        return res;
+    }
+
+    private void helper(char[] chars, List<String> res, int index) {
+        if (index == chars.length) {
+            res.add(new String(chars));
+            return;
+        }
+        if (chars[index] >= '0' && chars[index] <= '9') {
+            helper(chars, res, index + 1);
+            return;
+        }
+
+        chars[index] = Character.toLowerCase(chars[index]);
+        helper(chars, res, index + 1);
+
+        chars[index] = Character.toUpperCase(chars[index]);
+        helper(chars, res, index + 1);
+    }
+
+
+
+
     public List<String> letterCasePermutation(String S) {
         List<StringBuilder> res = new ArrayList<StringBuilder>();
         res.add(new StringBuilder());
