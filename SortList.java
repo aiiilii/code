e2439425d5443fb1dbcc9c06529280ed1933daa2
Aili Gong <aili.hgong@gmail.com>
2020-03-18 00:00:54 +0000

    Added SortList

---
+++ SortList.java
@@ -0,0 +1,68 @@
+public class SortList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    /**
+     * Time - O(n log n)
+     * Space - O(1)
+     * @param head
+     * @return
+     */
+    public ListNode sortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode[] list = new ListNode[2];
+        boolean done = (head == null);
+
+        // Keep partitioning our list into bigger sublists length. Starting with a size of 1 and doubling each time
+        for (int step = 1; !done; step*=2) {
+            done = true;
+            ListNode prev = dummy;
+            ListNode remaining = prev.next;
+
+            do {
+                // Split off two sublists of size step
+                for (int i = 0; i < 2; i++) {
+                    list[i] = remaining;
+                    ListNode tail = null;
+                    for (int j = 0; j < step && remaining != null; j ++, remaining = remaining.next) {
+                        tail = remaining;
+                    }
+                    // Terminate our sublist
+                    if (tail != null) {
+                        tail.next = null;
+                    }
+                }
+
+                // We're done if these are the first two sublists in this pass and they
+                // encompass the entire primary list
+                done &= (remaining == null);
+
+                // If we have two sublists, merge them into one
+                if (list[1] != null) {
+                    while (list[0] != null || list[1] != null) {
+                        int idx = (list[1] == null || list[0] != null && list[0].val <= list[1].val) ? 0 : 1;
+                        prev.next = list[idx];
+                        list[idx] = list[idx].next;
+                        prev = prev.next;
+                    }
+
+                    // Terminate our new sublist
+                    prev.next = null;
+                } else {
+                    // Only a single sublist, no need to merge, just attach to the end
+                    prev.next = list[0];
+                }
+            } while (remaining != null);
+        }
+        return dummy.next;
+    }
+}
