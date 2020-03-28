d3b2abb0d541b43f8b50f7334446341c3226270a
Aili Gong <aili.hgong@gmail.com>
2020-03-28 00:14:40 +0000

    Added second solution to MaximumProductSubarray

--- MaximumProductSubarray.java
+++ MaximumProductSubarray.java
@@ -1,7 +1,34 @@
 public class MaximumProductSubarray {
 
     /**
-     * Time - o(n)
+     * Time - O(n)
+     * Space - O(1)
+     * @param nums
+     * @return
+     */
+    public int maxProduct1(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int max = nums[0];
+        int min = nums[0];
+        int res = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            int temp = max;
+            max = Math.max(Math.max(max * nums[i], min + nums[i]), nums[i]);
+            min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+            if (max > res) {
+                res = max;
+            }
+        }
+        return res;
+    }
+
+
+
+    /**
+     * Time - O(n)
      * Space - O(1)
      * @param nums
      * @return
