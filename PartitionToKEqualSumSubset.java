4ef03330a04fcb275c265c8cf7323775704d816c
Aili Gong <aili.hgong@gmail.com>
2020-03-30 20:20:10 +0000

    Added PartitionToKEqualSubset

--- PartitionToKEqualSumSubset.java
+++ PartitionToKEqualSumSubset.java
@@ -13,17 +13,17 @@ public class PartitionToKEqualSumSubset {
         if (sum % k > 0) {
             return false;
         }
-        int target = sum / k;
+        int target = sum / k; // get the sum of each small group;
 
         Arrays.sort(nums);
 
         int row = nums.length - 1;
-        if (nums[row] > target) {
+        if (nums[row] > target) { // nums[row] is the largest number after sorting, if it is greater than target sum, then nowhere to put it, return false;
             return false;
         }
-        while (row >= 0 && nums[row] == target) {
-            row --;
-            k --;
+        while (row >= 0 && nums[row] == target) { //  if the largest number equal to targer sum,
+            row --; // decrement last index;
+            k --; // decrement the k groups;
         }
         return search(new int[k], row, nums, target);
     }
@@ -32,12 +32,12 @@ public class PartitionToKEqualSumSubset {
         if (row < 0) {
             return true;
         }
-        int v = nums[row];
+        int v = nums[row]; // current largest number;
         row --;
 
         for (int i = 0; i < groups.length; i++) {
-            if (groups[i] + v <= target) {
-                groups[i] += v;
+            if (groups[i] + v <= target) { 
+                groups[i] += v; // put number v into group bucket;
                 if (search(groups, row, nums, target)) {
                     return true;
                 }
