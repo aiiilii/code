5d7aaa5effc0871828aa21fe3215680545cca625
Aili Gong <aili.hgong@gmail.com>
2020-03-30 17:49:50 +0000

    Finished FindAllDuplicatesInAnArray

--- FindAllDuplicatesInAnArray.java
+++ FindAllDuplicatesInAnArray.java
@@ -4,7 +4,8 @@ import java.util.List;
 public class FindAllDuplicatesInAnArray {
 
     /**
-     * When find a number i, flip the number at position i-1 to negative. 
+     * The question specifies that 1 <= a[i] <= n, in an n-sized array, thus the below solution could work.
+     * When find a number i, flip the number at position i-1 to negative. This is because position is zero-based.
      * If the number at position i-1 is already negative, i is the number that occurs twice.
      * Time - O(n)
      * Space - O(1)
@@ -18,9 +19,9 @@ public class FindAllDuplicatesInAnArray {
         }
 
         for (int i = 0; i < nums.length; i++) {
-            int index = Math.abs(nums[i]) - 1;
+            int index = Math.abs(nums[i]) - 1; // use Math.abs to get the true value of the index, because nums[i] might have been changed to negative;
             if (nums[index] < 0) {
-                res.add(index + 1); // index could never be negative;
+                res.add(index + 1); // index could never be negative, and index + 1 is convert the index back to nums[i];
             }
             nums[index] = -nums[index];
         }
