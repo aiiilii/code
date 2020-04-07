385f2eed12b20c70fb4f79d422d4a59dfa1f6e06
Aili Gong <aili.hgong@gmail.com>
2020-04-07 18:29:40 +0000

    Added treemap solution to relativeSortArray

--- RelativeSortArray.java
+++ RelativeSortArray.java
@@ -1,6 +1,11 @@
+import java.util.TreeMap;
+
 public class RelativeSortArray {
 
     /**
+     * Counting sort approach
+     * Because 0 <= arr1[i], arr2[i] <= 1000, we use an array to count every element.
+     * Go through every element in the second array, and update values of the first array based on the order in the second array.
      * Time - O(n)
      * Space - O(1)
      * @param arr1
@@ -31,6 +36,42 @@ public class RelativeSortArray {
             }
         }
 
+        return arr1;
+    }
+
+
+
+    /**
+     * TreeMap approach
+     * If contraint 0 <= arr1[i], arr2[i] <= 1000 does not exist.
+     * Time - O(n log n)
+     * Space - O(n)
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    public int[] relativeSortArray1(int[] arr1, int[] arr2) {
+        TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+        for (int n : arr1) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        int i = 0;
+        for (int n : arr2) {
+            while (map.get(n) > 0) {
+                arr1[i] = n;
+                i ++;
+                map.put(n, map.get(n) - 1);
+            }
+        }
+
+        for (int n : map.keySet()) {
+            while (map.get(n) > 0) {
+                arr1[i] = n;
+                i ++;
+                map.put(n, map.get(n) - 1);
+            }
+        }
         return arr1;
     }
 }
