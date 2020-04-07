2757111e131ca1a0e49d04fda576112cd2bc7de7
Aili Gong <aili.hgong@gmail.com>
2020-04-07 18:59:42 +0000

    Finished notes for FlipAnImage

--- FlipAnImage.java
+++ FlipAnImage.java
@@ -1,6 +1,10 @@
 public class FlipAnImage {
 
     /**
+     * One pass
+     * For each row, use two pointers. One going forward and another going backwards.
+     * 1. If the two elements are the same, then make a light change: 0 -> 1 or 1 -> 0
+     * 2. If the two elements are different, DON'T do anything. Continue.
      * Time - O(n)
      * Space - O(1)
      * @param A
