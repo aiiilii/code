72b6f31926d3b572fdb9e31c24b2a839f7687cb7
Aili Gong <aili.hgong@gmail.com>
2020-04-15 00:03:49 +0000

    Finished ReverseWordsInAString

--- ReverseWordsInAString.java
+++ ReverseWordsInAString.java
@@ -1,6 +1,40 @@
+import java.util.LinkedList;
+
 public class ReverseWordsInAString {
 
+    /**
+     * Time - O(n)
+     * Space - O(n)
+     * @param s
+     * @return
+     */
     public String reverseWordS(String s) {
-        
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left <= right && s.charAt(left) == ' ') {
+            left ++;
+        }
+        while (left <= right && s.charAt(right) == ' ') {
+            right --;
+        }
+
+        LinkedList<String> list = new LinkedList<String>();
+        StringBuilder word = new StringBuilder();
+
+        while (left <= right) {
+            char c = s.charAt(left);
+
+            if (word.length() != 0 && c == ' ') {
+                list.offerFirst(word.toString());
+                word = new StringBuilder();
+            } else if (c != ' ') {
+                word.append(c);
+            }
+            left ++;
+        }
+        list.offerFirst(word.toString());
+
+        return String.join(" ", list);
     }
 }
