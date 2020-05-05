32496203197f1643a4eba7f92d93f93fb230c964
Aili Gong <aili.hgong@gmail.com>
2020-05-04 19:53:47 -0700

    Added VerticalOrderTraversalOfBinaryTree

---
+++ VerticalOrderTraversalOfBinaryTree.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class VerticalOrderTraversalOfBinaryTree {
+    
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode (int x) { val = x; }
+    }
+
+    public List<List<Integer>> vertivalTraversal(TreeNode root) {
+        
+    }
+}
