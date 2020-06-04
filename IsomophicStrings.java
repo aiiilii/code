d7583cb24762422ba69abe084c00a2833057acec
Aili Gong <aili.hgong@gmail.com>
2020-06-04 14:14:25 -0700

    Added IsomophicStrings

--- IsomophicStrings.java
+++ IsomophicStrings.java
@@ -1,4 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsomophicStrings {
     
-    
+    /**
+     * Time - O(n)
+     * Space - O(n)
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isIsomorphic(String s, String t) {
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> mapS = new HashMap<Character, Integer>();
+        Map<Character, Integer> mapT = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < s.length(); i++) {
+            int indexS = mapS.getOrDefault(s.charAt(i), -1);
+            int indexT = mapT.getOrDefault(t.charAt(i), -1);
+
+            if (indexS != indexT) {
+                return false;
+            }
+
+            mapS.put(s.charAt(i), i);
+            mapT.put(t.charAt(i), i);
+        }
+        return true;
+    }
 }
