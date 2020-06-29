bbdbbd3b09f08efc0ccce10d077fe052042b8763
Aili Gong <aili.hgong@gmail.com>
2020-06-29 10:16:51 -0700

    Finished PartitionLabels

--- PartitionLabels.java
+++ PartitionLabels.java
@@ -1,8 +1,27 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class PartitionLabels {
     
-    public List<Integer> partitionLabels(String s) {
+    public List<Integer> partitionLabels(String S) {
+        int[] last = new int[26];
         
+        for (int i = 0; i < S.length(); i++) {
+            last[S.charAt(i) - 'a'] = i;
+        }
+
+        int j = 0;
+        int anchor = 0;
+        List<Integer> res = new ArrayList<Integer>();
+        
+        for (int i = 0; i < S.length(); i++) {
+            j = Math.max(j, last[S.charAt(i) - 'a']);
+            if (i == j) {
+                res.add(i - anchor + 1);
+                anchor = i + 1;
+            }
+        }
+
+        return res;
     }
 }
